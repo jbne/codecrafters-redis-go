dc31a17cef52a8de61c09635fb5b17eec543d994
kimaluco17 <34797456+kimaluco17@users.noreply.github.com>
2026-01-27 21:46:43 -0500

    handle concurrent clients

--- app/server.go
+++ app/server.go
@@ -6,6 +6,27 @@ import (
 	"os"
 )
 
+func worker(conn net.Conn) {
+	fmt.Printf("Client connected! conn: %s\n", conn.RemoteAddr())
+	defer conn.Close()
+
+	for {
+		buf := make([]byte, 1024)
+		_, err := conn.Read(buf)
+		if err != nil {
+			fmt.Printf("Read err: %v\n", err)
+			return
+		}
+
+		fmt.Printf("%s: %s\n", conn.RemoteAddr(), buf)
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			fmt.Printf("Write err: %v\n", err)
+			return
+		}
+	}
+}
+
 func main() {
 	address := "127.0.0.1"
 	port := "6379"
@@ -18,30 +39,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println("Waiting for client connection...")
-	conn, err := listener.Accept()
-	listener.Close()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
-	fmt.Println("Client connected!")
-	defer conn.Close()
-
-	buf := make([]byte, 1024)
 	for {
-		_, err := conn.Read(buf)
+		fmt.Println("Waiting for client connection...")
+		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Printf("Read err: %v\n", err)
+			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-
-		fmt.Printf("%s\n", buf)
-		conn.Write([]byte("+PONG\r\n"))
-		if err != nil {
-			fmt.Printf("Write err: %v\n", err)
-			os.Exit(1)
-		}
+		go worker(conn)
 	}
 }
